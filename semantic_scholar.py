ead7febb1ca6bf6918b769112e92805afbca20d1
SummCoder <2404174187@qq.com>
2024-05-23 16:16:06 +0800

    初步构建

---
+++ semantic_scholar.py
@@ -0,0 +1,82 @@
+import requests
+import pandas as pd
+from utils.glm4 import glm_analyse
+import re
+
+df = pd.DataFrame(columns=["Type", "Title", "Description", "Classification"])
+
+# 修改为相应论文的id
+semantic_scholar_id = "fa63131faa1e2b802bee2c4e926899002c65e203"
+
+# 构造请求的URL
+url = f"https://api.semanticscholar.org/v1/paper/{semantic_scholar_id}"
+
+# 发起请求
+response = requests.get(url)
+
+# 检查请求是否成功
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+    print("Title======================================")
+    print(data["title"])
+    print("\n\n")
+    references_data = [{"Type": "Title", "Title": data.get('title', 'No title available'), "Description": "initial",
+                        "Classification": 1}]
+    df = pd.concat([df, pd.DataFrame(references_data)], ignore_index=True)
+
+    # 检查是否有引用文献
+    if "references" in data and len(data["references"]) > 0:
+        # 打印引用文献的信息，例如标题、作者、时间和Semantic Scholar-ID
+        print("References=================================")
+        for reference in data["references"]:
+            # print(f"Title: {reference.get('title', 'No title available')}")
+            print(f"{reference.get('title', 'No title available')}")
+            url0 = reference.get('url').split("paper/")[-1]
+            url1 = f"https://api.semanticscholar.org/v1/paper/{url0}"
+            response0 = requests.get(url1).json()["abstract"]
+            result = glm_analyse(response0)
+            # 构建字典存储结果数据
+            response_data = result.choices[0].message.content
+            score = re.findall(r'\d+', response_data)[-1]
+            references_data = [{"Type": "Reference", "Title": reference.get('title', 'No title available'),
+                                "Description": response_data, "Classification": score}]
+            df = pd.concat([df, pd.DataFrame(references_data)], ignore_index=True)
+            # # 打印每个引用的作者，如果有的话
+            # if "authors" in reference:
+            #     authors = ", ".join([author.get("name", "N/A") for author in reference["authors"]])
+            #     print(f"Authors: {authors}")
+            # # 打印出版年份
+            # print(f"Year: {reference.get('year', 'No year available')}")
+            # # 打印 Semantic Scholar ID
+            # print(f"Semantic Scholar ID: {reference.get('paperId', 'No ID available')}")
+            # print("-----")
+
+    else:
+        print("暂没有 References")
+    print("\n\n")
+    if "citations" in data and len(data["citations"]) > 0:
+        print("Citations=================================")
+        for citation in data["citations"]:
+            # print(f"Title: {reference.get('title', 'No title available')}")
+            print(f"{citation.get('title', 'No title available')}")
+            url0 = citation.get('url').split("paper/")[-1]
+            url1 = f"https://api.semanticscholar.org/v1/paper/{url0}"
+            response0 = requests.get(url1).json()["abstract"]
+            result = glm_analyse(response0)
+            # 构建字典存储结果数据
+            response_data = result.choices[0].message.content
+            score = re.findall(r'\d+', response_data)[-1]
+            citations_data = [{"Type": "Citation", "Title": citation.get('title', 'No title available'),
+                               "Description": response_data, "Classification": score}]
+            df = pd.concat([df, pd.DataFrame(citations_data)], ignore_index=True)
+    else:
+        print("暂没有 Citations")
+
+    print("\n\n")
+    # 将DataFrame写入CSV文件
+    df.to_csv("result.csv", index=False)
+
+    print("数据已成功写入到result.csv文件中。")
+else:
+    print(f"请求失败，状态码：{response.status_code}")
---
+++ utils/glm4.py
@@ -0,0 +1,38 @@
+# 基于glm4进行需要滚雪球文章的分类
+
+from zhipuai import ZhipuAI
+
+client = ZhipuAI(api_key="")
+
+
+def glm_analyse(content):
+    tools = [{
+        "type": "web_search",git g
+        "web_search": {
+            "enable": False,
+            "search_result": False,  # 禁用False，启用：True，默认为禁用
+        }
+    }]
+    messages = [
+        {
+            "role": "system",
+            "content": "你是一个乐于助人的论文阅读专家，热心解答用户提供的相关论文的分析"
+        },
+        {
+            "role": "user",
+            "content": """根据以下论文的摘要内容""" + content + """。\n"
+                       + "你应该按照下列方式进行思考：\n"
+                         "1.总结该篇文章内容\n"
+                         "2.然后判断是否是关于代码模型任务而非其他领域例如nlp领域，注意与自然语言处理相区分，要实际涉及到代码相关任务\n"
+                         "3.接着分析是否涉及针对代码任务模型安全领域如攻击和防御\n"
+                         "4.最后给出一个**综合分类**，第二点和第三点都是的话，输出1，否则0，注意最后只有一位数字0或者1"
+                         """
+        }]
+
+    response = client.chat.completions.create(
+        model="glm-4",
+        messages=messages,
+        tools=tools
+    )
+    return response
+
